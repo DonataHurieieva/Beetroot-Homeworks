0c4721afbe3f2cd97deef724bda462dcf819bd25
Donata Hurieieva <donatagureeva@gmail.com>
2023-03-03 18:23:43 +0200

    home task

---
+++ Lesson1/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+
+{
+public static void Main(String[] args)
+
+{
+   Console.WriteLine("Write x value");
+   
+  float x = Convert.ToSingle(Console.ReadLine());
+
+int cube = (int)Math.Pow(x,3);
+int square = (int)Math.Pow(x,2);
+Math.Max(x, 3);
+
+float equationResult = -6 * cube + 5 *square - 10 * x + 15;
+Console.WriteLine("-6x^3 + 5x^2 - 10x + 15 = " + equationResult);
+double sinResult = Math.Abs(x) * Math.Sin(x);
+Console.WriteLine("abs(x) * sin(x) = " + sinResult);
+double piResult = 2 * Math.PI * x;
+Console.WriteLine("2 * PI * x = " + piResult);
+
+Console.WriteLine("enter y value");
+float y = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("max value is "+ (Math.Max(x,y)));
+
+}
+
+}
